c14221a6f9cf814269f7384bde4df1930ca70548
WebMats <lorlando127@gmail.com>
2019-02-26 16:26:15 -0500

    added session support

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+
+const sessionConfig = {
+    name: 'lambda-authentication',
+    secret: process.env.COOKIE_SECRET,
+    cookie: { maxAge: 1000 * 60 * 20 },
+    resave: false,
+    saveUninitialized: false
+}
 
 const app = express();
 const authGuard = require('./middleware/auth');
 app.use(express.json())
 app.use(helmet());
 app.use(cors());
+app.use(session(sessionConfig));
 
 const userRouter = require('./router/user');
 const authRouter = require('./router/auth');
